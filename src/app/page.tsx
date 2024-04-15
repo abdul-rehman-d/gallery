245a719723cd25cc7d1efd4b6c580351b2ffef87
Abdul Rehman <ar618211@gmail.com>
2024-04-15 13:20:48 +0500

    removed mock urls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,6 @@ import { db } from "@/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockURLs = [
-  "https://utfs.io/f/7379636d-36e1-4727-86fb-9f67085890e2-5n3gjp.jpg",
-  "https://utfs.io/f/270db83a-7d54-4dda-b604-825c331b6cc2-2rqyil.jpg",
-  "https://utfs.io/f/c7867d8d-1564-43c1-963f-2f6c0eca1ddb-i71y02.jpg",
-];
-
 export default async function HomePage() {
   const imagesFromDB = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.createdAt),
