f5521e7a1f68e2411d59653e2a9488f5dce130d6
Abdul Rehman <ar618211@gmail.com>
2024-04-16 00:57:50 +0500

    title ui fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,9 @@ async function Images() {
   return (
     <div className="flex flex-wrap gap-4">
       {imagesFromDB.map(image => (
-        <div className="w-48 flex flex-col items-center gap-2" key={image.id}>
+        <div className="w-48 flex flex-col items-center gap-2 overflow-hidden" key={image.id}>
           <img src={image.url} className="w-48 h-40 object-cover" />
-          <span>{image.name}</span>
+          <span className="max-w-full text-center">{image.name}</span>
         </div>
       ))}
     </div>
