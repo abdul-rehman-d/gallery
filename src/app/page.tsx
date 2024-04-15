fab791c096b097c61b442924e7bb4d99b1e6ef31
Abdul Rehman <ar618211@gmail.com>
2024-04-15 13:18:13 +0500

    db change and query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,22 +8,17 @@ const mockURLs = [
   "https://utfs.io/f/c7867d8d-1564-43c1-963f-2f6c0eca1ddb-i71y02.jpg",
 ];
 
-const mockImages = mockURLs.map((url, idx) => {
-  return {
-    id: idx,
-    url,
-  };
-});
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log("posts ->", posts)
+  const imagesFromDB = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.createdAt),
+  });
   return (
     <main className="p-4">
       <div className="flex flex-wrap gap-4">
-        {mockImages.map(image => (
-          <div className="w-48 aspect-video" key={image.id}>
-            <img src={image.url} className="aspect-video" />
+        {imagesFromDB.map(image => (
+          <div className="w-48 flex flex-col items-center gap-2" key={image.id}>
+            <img src={image.url} className="w-48 h-40 object-cover" />
+            <span>{image.name}</span>
           </div>
         ))}
       </div>
