cea18ac8872fdede0a5582d131a32da33f496a59
Abdul Rehman <ar618211@gmail.com>
2024-04-18 09:08:17 +0500

    loading spinner

--- src/components/simple-upload-button.tsx
+++ src/components/simple-upload-button.tsx
@@ -48,6 +48,25 @@ function UploadSvg() {
   );
 }
 
+function LoadingSpinner() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <circle className="spinner_DupU" cx="12" cy="3" r="0"/>
+      <circle className="spinner_DupU spinner_GWtZ" cx="16.50" cy="4.21" r="0"/>
+      <circle className="spinner_DupU spinner_n0Yb" cx="7.50" cy="4.21" r="0"/>
+      <circle className="spinner_DupU spinner_dwN6" cx="19.79" cy="7.50" r="0"/>
+      <circle className="spinner_DupU spinner_GIL4" cx="4.21" cy="7.50" r="0"/>
+      <circle className="spinner_DupU spinner_46QP" cx="21.00" cy="12.00" r="0"/>
+      <circle className="spinner_DupU spinner_DQhX" cx="3.00" cy="12.00" r="0"/>
+      <circle className="spinner_DupU spinner_PD82" cx="19.79" cy="16.50" r="0"/>
+      <circle className="spinner_DupU spinner_tVmX" cx="4.21" cy="16.50" r="0"/>
+      <circle className="spinner_DupU spinner_eUgh" cx="16.50" cy="19.79" r="0"/>
+      <circle className="spinner_DupU spinner_j38H" cx="7.50" cy="19.79" r="0"/>
+      <circle className="spinner_DupU spinner_eUaP" cx="12" cy="21" r="0"/>
+    </svg>
+  );
+}
+
 export function SimpleUploadButton() {
   const router = useRouter();
   let toastId: string | number;
@@ -69,7 +88,7 @@ export function SimpleUploadButton() {
     <div>
       <label htmlFor="file-upload" className="cursor-pointer">
         <div className={`bg-white p-1 rounded-md text-black ${isUploading ? "bg-white/50" : "" }`}>
-          <UploadSvg />
+          {isUploading ? <LoadingSpinner /> : <UploadSvg />}
         </div>
       </label>
       <input
